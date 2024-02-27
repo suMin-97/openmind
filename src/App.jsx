3f787b6d41321f7d261e4b487f8632b71bd14d1c
aoooec <foottable@gmail.com>
2024-02-27 18:41:44 +0900

    design(reset): reset 스타일  적용
    reset.css를 스타일드 컴포넌트 글로벌 스타일을 이용해 적용했습니다.
    
    X

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
-  return <div>안녕하세요 늦어서 죄송합니다.</div>;
+  return (
+    <div>
+      <GlobalStyle />
+      안녕하세요 늦어서 죄송합니다.
+    </div>
+  );
 }
 
 export default App;
