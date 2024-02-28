9e3f69c6bb805e3591bc5852e60dae4aa8320ba2
aoooec <foottable@gmail.com>
2024-02-28 17:20:56 +0900

    fix(fontStyles): 세미콜론 수정 (, -> ;)
    실수로 잘못 입력한 부분을 수정했습니다.
    
    X

--- src/styles/fontStyles.js
+++ src/styles/fontStyles.js
@@ -4,9 +4,9 @@ const fontStyles = {
   h3: "font-size: 1.5rem; line-height: 1.25",
   body1: "font-size: 1.25rem; line-height: 1.25",
   body1: "font-size: 1.25rem; line-height: 1.25",
-  body2: "font-size: 1.125rem, line-height: 1.333",
-  body3: "font-size: 1rem, line-height: 1.375",
-  caption: "font-size: 0.875rem, line-height: 1.285",
+  body2: "font-size: 1.125rem; line-height: 1.333",
+  body3: "font-size: 1rem; line-height: 1.375",
+  caption: "font-size: 0.875rem; line-height: 1.285",
 
   /* font-weight */
   bold: "font-weight: bold",
