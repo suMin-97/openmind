bfde5afcb84569102dc0cba8fef4f642e113b8f6
aoooec <112041983+aoooec@users.noreply.github.com>
2024-02-28 15:43:15 +0900

    style(App): 동일한 곳에서의 import 요소들 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import GlobalFont from "./styles/fonts/fonts";
 import MainPage from "./Pages/MainPage";
