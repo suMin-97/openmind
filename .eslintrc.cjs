3826d5b5f36c7bcda335f8146f7849249f31dfab
aoooec <foottable@gmail.com>
2024-02-29 14:46:41 +0900

    chore: eslint 룰 추가
    화살표 함수 사용과 단축 절대경로(@~) 사용에 오류 표기가 되지 않도록 룰을
    추가했습니다.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,47 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended', "airbnb", "plugin:prettier/recommended"],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    react: { version: "18.2" },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx"],
+      },
+      alias: {
+        map: [["~", path.resolve(__dirname, "./src")]],
+        extensions: [".js", ".jsx"],
+      },
+    },
+  },
+  plugins: ["react-refresh"],
   rules: {
     "prettier/prettier": [
       "error",
       {
-        "endOfLine": "auto"
-      }
+        endOfLine: "auto",
+      },
     ],
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/function-component-definition": [
+      2,
+      { namedComponents: ["arrow-function", "function-declaration"] },
+    ],
+    "react/react-in-jsx-scope": "off",
   },
-}
+};
