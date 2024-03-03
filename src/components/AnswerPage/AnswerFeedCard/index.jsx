2b35625101426dff7768d5a93cfbeed806feafe7
aoooec <foottable@gmail.com>
2024-03-04 03:01:59 +0900

    add(AnswerFeedCard): 답변하기 페이지용 피드카드 컴포넌트 생성
    기존 FeedCard 컴포넌트 활용하여 답변하기 페이지용 피드카드 컴포넌트
    생성.
    디자인 적용하지 않음
    논의 통해 기존 FeedCard 컴포넌트도 수정이 필요할 것 같습니다. (베이스가
    되는 FeedCard 컴포넌트는 일단 손대지 않음)

---
+++ src/components/AnswerPage/AnswerFeedCard/index.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import FeedPageProfile from "../../FeedPage/FeedPageProfile";
+import Reaction from "@components/common/Reaction";
+import getTimeDiff from "@components/common/FeedCard/getTimeDiff";
+import Badge from "../../common/Badge";
+import InputTextareaForm from "../../common/InputTextAreaForm";
+import useRequest from "../../../hooks/useRequest";
+import MoreDropdown from "../MoreDropdown";
+
+const ContainDiv = styled.div`
+  width: 600px;
+  height: 500px;
+  margin: 10px;
+  border: 3px solid black;
+`;
+
+const FeedCard = ({ feedCardData, isLoading }) => {
+  const {
+    id: questionId,
+    content: questionContent,
+    subjectId,
+    like,
+    dislike,
+    createdAt,
+    answer,
+  } = feedCardData ?? {};
+
+  const {
+    data: submitAnswerResponse,
+    isLoading: submitAnswerLoading,
+    error,
+    request: useSubmitAnswer,
+  } = useRequest({
+    url: `questions/${questionId}/answers`,
+    method: "POST",
+  });
+
+  const handleSubmitAnswer = (value) => {
+    useSubmitAnswer({ content: value, isRejected: false });
+  };
+
+  return (
+    <ContainDiv>
+      {/* 답변완료 */}
+      <div>
+        {feedCardData && answer ? <Badge isAnswered /> : <Badge />}
+        <MoreDropdown isAnswered={answer} />
+      </div>
+
+      {/* 질문 */}
+      {feedCardData && (
+        <div>
+          <p>{getTimeDiff(createdAt)}</p>
+          <p>{questionContent}</p>
+        </div>
+      )}
+      {/* 답변 */}
+      {feedCardData && answer ? (
+        <div>
+          <FeedPageProfile subjectId={subjectId} />
+          {answer?.isRejected ? (
+            <p style={{ color: "red" }}>답변거절</p>
+          ) : (
+            <p>{answer?.content}</p>
+          )}
+        </div>
+      ) : (
+        <InputTextareaForm
+          formType="answer"
+          handleSubmit={handleSubmitAnswer}
+        />
+      )}
+      <Reaction
+        questionId={feedCardData?.id}
+        like={feedCardData?.like}
+        dislike={feedCardData?.dislike}
+      />
+    </ContainDiv>
+  );
+};
+
+export default FeedCard;
