ce632abaee86c5266b051b15734bb2c60cabfb70
Sumin <csmxx99@naver.com>
2024-03-03 11:47:16 +0900

    feat: id 값의 변화에 따른 유동적 작업을 위해 메인 페이지에 임시 로그인 추가함

--- src/Pages/FeedPage/index.jsx
+++ src/Pages/FeedPage/index.jsx
@@ -5,8 +5,10 @@ import ShareButtons from "../../components/common/ShareButtons";
 import { useLocation } from "react-router-dom";
 import { BASIC_DEPLOY_URL } from "../../constants/constants";
 import { useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-const FeedPage = ({ subjectId = 3859 }) => {
+const FeedPage = () => {
+  const { id } = useParams();
   const currentLocation = useLocation();
   const currentPath = currentLocation.pathname;
   const currentUrl = `${BASIC_DEPLOY_URL}${currentPath}`;
@@ -14,13 +16,13 @@ const FeedPage = ({ subjectId = 3859 }) => {
 
   return (
     <div>
-      <FeedPageProfile subjectId={subjectId} />
+      <FeedPageProfile subjectId={id} />
       <Link to="/">메인 페이지</Link>
       <Link to="/list">리스트 페이지</Link>
-      <Link to="/post/1/answer">답변 페이지</Link>
+      <Link to={`/post/${id}/answer`}>답변 페이지</Link>
       <ShareButtons url={currentUrl} />
       <Modalsection
-        subjectId={subjectId}
+        subjectId={id}
         searchParams={searchParams}
         setSearchParams={setSearchParams}
       />
--- src/Pages/MainPage/index.jsx
+++ src/Pages/MainPage/index.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const MainPage = () => {
+  // 임시 로그인으로 피드, 답변 페이지에 ID 값 부여함, 메인 페이지 제작 되면 삭제 요망
+  const [id, setId] = useState(0);
+
   return (
     <div>
+      <form>
+        <label>
+          ID 값 넣어!!!!!!!!
+          <input type="number" />
+        </label>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            setId(event.target.form[0].value);
+          }}
+        >
+          입력
+        </button>
+      </form>
       <h1>메인 페이지</h1>
       <Link to="/list">리스트 페이지</Link>
-      <Link to="/post/1">피드 페이지</Link>
-      <Link to="/post/1/answer">답변 페이지</Link>
+      <Link to={`/post/${id}`}>피드 페이지</Link>
+      <Link to={`/post/${id}/answer`}>답변 페이지</Link>
     </div>
   );
 };
