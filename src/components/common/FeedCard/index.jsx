3083929d4324cb5cfe345bc33d954c28ef26b775
aoooec <foottable@gmail.com>
2024-03-04 02:56:48 +0900

    style(FeedCard): getTimeDiff 유틸 분리
    FeedCard 컴포넌트 내부에 선언되어있던 timeDiff 함수를 별도 유틸 함수로
    변경(동시에 이름도 getTimeDiff로 변경하였습니다)
    사용하지 않는 훅 import 삭제하였습니다.

---
+++ src/components/common/FeedCard/index.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import FeedPageProfile from "../../FeedPage/FeedPageProfile";
+import Reaction from "../Reaction";
+import getTimeDiff from "./getTimeDiff";
+
+const ContainDiv = styled.div`
+  width: 600px;
+  height: 500px;
+  margin: 10px;
+  border: 3px solid black;
+`;
+
+const FeedCard = ({ feedCard, isLoading, error }) => {
+  return (
+    <ContainDiv>
+      {/* 답변완료 */}
+      {isLoading && <p>로딩중입니다</p>}
+      {feedCard && feedCard?.answer ? <div>답변 완료</div> : <div>미답변</div>}
+      {error && <p>삐빅 에러 입니다</p>}
+      {/* 질문 */}
+      {isLoading && <p>로딩중입니다</p>}
+      {feedCard && (
+        <div>
+          <p>{getTimeDiff(feedCard?.createdAt)}</p>
+          <p>{feedCard?.content}</p>
+        </div>
+      )}
+      {error && <p>삐빅 에러 입니다</p>}
+      {/* 답변 */}
+      {isLoading && <p>로딩중입니다</p>}
+      {feedCard && feedCard?.answer ? (
+        <div>
+          <FeedPageProfile subjectId={feedCard?.subjectId} />
+          {feedCard?.answer?.isRejected ? (
+            <p style={{ color: "red" }}>답변거절</p>
+          ) : (
+            <p>{feedCard?.answer?.content}</p>
+          )}
+        </div>
+      ) : null}
+      {error && <p>삐빅 에러 입니다</p>}
+      <Reaction
+        questionId={feedCard?.id}
+        like={feedCard?.like}
+        dislike={feedCard?.dislike}
+      />
+    </ContainDiv>
+  );
+};
+
+export default FeedCard;
