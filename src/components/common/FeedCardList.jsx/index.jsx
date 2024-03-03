d01058dbab2e9caefa9a571e1fad924f7c25ab4b
Sumin <csmxx99@naver.com>
2024-03-03 19:14:22 +0900

    feat: FeedCardList의 질문 갯수 카운터, 질문 없을 때 별도 이미지 표출

--- src/components/common/FeedCardList.jsx/index.jsx
+++ src/components/common/FeedCardList.jsx/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import useRequest from "../../../hooks/useRequest.js";
 import FeedCard from "../FeedCard.jsx/index.jsx";
+import NoFeedCard from "../NoFeedCard/index.jsx";
 
 const FeedCardList = ({ subjectId }) => {
   const FeedCardsUrl = `subjects/${subjectId}/questions`;
@@ -17,19 +18,31 @@ const FeedCardList = ({ subjectId }) => {
 
   return (
     <div>
-      <ul>
-        <li>
-          {feedCards &&
-            feedCards?.results.map((feedCard) => (
-              <FeedCard
-                key={feedCard?.id}
-                feedCard={feedCard}
-                error={error}
-                isLoading={isLoading}
-              />
-            ))}
-        </li>
-      </ul>
+      {isLoading && <p>로딩중</p>}
+      {feedCards && feedCards?.count ? (
+        <>
+          <h1>{`${feedCards?.count}개의 질문이 있습니다`}</h1>
+          <ul>
+            <li>
+              {feedCards &&
+                feedCards?.results.map((feedCard) => (
+                  <FeedCard
+                    key={feedCard?.id}
+                    feedCard={feedCard}
+                    error={error}
+                    isLoading={isLoading}
+                  />
+                ))}
+            </li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <p>아직 질문이 없습니다</p>
+          <NoFeedCard />
+        </>
+      )}
+      {error && <p>삐빅 에러 입니다</p>}
     </div>
   );
 };
