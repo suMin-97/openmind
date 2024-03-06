61012e2298bd562875e6a0d47bf58274c5b6b10e
aoooec <foottable@gmail.com>
2024-03-06 13:34:27 +0900

    fix: fonts import 변경
    적용이 제대로 안되는 것 같아서 변경했습니다.

--- src/styles/fonts/fonts.js
+++ src/styles/fonts/fonts.js
@@ -1,22 +1,19 @@
 import { createGlobalStyle } from "styled-components";
-import PretendardBold from "./Pretendard-Bold.woff2";
-import PretendardMedium from "./Pretendard-Medium.woff2";
-import PretendardRegular from "./Pretendard-Regular.woff2";
 
 const GlobalFont = createGlobalStyle`
   @font-face {
     font-family: "Pretendard";
-    src: local("Pretendard"), url(${PretendardRegular}) format("woff2");
+    src: local("Pretendard"), url("./Pretendard-Regular.woff2"}) format("woff2");
     font-weight: 400;
   }
   @font-face {
     font-family: "Pretendard";
-    src: local("Pretendard"), url(${PretendardMedium}) format("woff2");
+    src: local("Pretendard"), url"./Pretendard-Medium.woff2") format("woff2");
     font-weight: 500;
   }
   @font-face {
     font-family: "Pretendard";
-    src: local("Pretendard"), url(${PretendardBold}) format("woff2");
+    src: local("Pretendard"), url("./Pretendard-Bold.woff2") format("woff2");
     font-weight: 700;
   }
 `;
