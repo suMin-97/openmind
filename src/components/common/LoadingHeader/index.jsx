1b956395fc3433dff5461b966d47e15e19aa5412
KimTaeEun1632 <qpwoal1324@naver.com>
2024-03-09 11:16:53 +0900

    fix: 헤더 스켈레톤 수정
    -네임박스 쉐도우 추가
    -스켈레톤 애니매이션을 넣어도 적용이 안되는데 어떻게 해야할까요??

--- src/components/common/LoadingHeader/index.jsx
+++ src/components/common/LoadingHeader/index.jsx
@@ -16,8 +16,13 @@ const LoadingProfileImg = styled.div`
   width: 104px;
   height: 104px;
   ${boxStyles.radiusC};
+  ${boxStyles.padding24};
+  ${boxStyles.shadow1};
   background-color: ${colors.loading};
 
+  &.skeleton {
+    ${boxStyles.skeletonAnimation}
+  }
   @media ${devices.tablet} {
     width: 136px;
     height: 136px;
@@ -30,9 +35,15 @@ const NameBox = styled.div`
 `;
 
 const LoadingProfileName = styled.div`
+  &.skeleton {
+    ${boxStyles.skeletonAnimation}
+  }
+
   background-color: ${colors.loading};
+  ${boxStyles.radius16};
   width: 75px;
   height: 20px;
+  ${boxStyles.shadow1};
 `;
 
 export default LoadingHeader;
