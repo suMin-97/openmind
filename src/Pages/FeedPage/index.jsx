b8c63eac0eddf9c902cb0fac7494f38006a3c3fb
KimTaeEun1632 <qpwoal1324@naver.com>
2024-03-09 19:38:21 +0900

    test: 무한스크롤 작업중  offet 증가시켜도 받아온 데이터가 변하지 않음

--- src/Pages/FeedPage/index.jsx
+++ src/Pages/FeedPage/index.jsx
@@ -1,20 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import FeedLayout from "@layout/FeedLayout";
 import useRequest from "@hooks/useRequest";
 import BASIC_QUESTION from "@components/FeedPage/ModalForm/constant";
 import ModalSection from "@components/FeedPage/ModalSection";
 import FeedContainer from "@components/common/FeedContainer";
+import styled from "styled-components";
 
 const FeedPage = () => {
   const { id } = useParams();
+  const [hasNext, setHasNext] = useState(true);
+  const target = useRef();
+  const offset = useRef(0);
+  const location = useLocation();
+
+  const LIMIT = 5;
 
   const {
     data: feedCardData,
     isLoading,
     error,
     request: getFeedCardData,
-  } = useRequest({ method: "GET", url: `subjects/${id}/questions` });
+  } = useRequest({
+    method: "GET",
+    url: `subjects/${id}/questions/?limit=${LIMIT}&offset=${offset.current}`,
+  });
 
   const { data: postQuestionResponse, request: postQuestion } = useRequest({
     url: `subjects/${id}/questions`,
@@ -29,15 +40,35 @@ const FeedPage = () => {
 
   useEffect(() => {
     getFeedCardData();
-  }, []);
+    offset.current += LIMIT;
+  }, [offset.current]);
 
-  const { count, results: feedCardList } = feedCardData ?? {};
+  const { count, next, results: feedCardList } = feedCardData ?? {};
 
   useEffect(() => {
     if (postQuestionResponse) {
       getFeedCardData();
     }
   }, [postQuestionResponse]);
+
+  // const observeCallback = (entries) => {
+  //   if (isLoading) return;
+
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting) {
+  //       getFeedCardData(offset.current);
+  //       setHasNext(next);
+  //     }
+  //   });
+  // };
+
+  // const observer = new IntersectionObserver(observeCallback, {
+  //   threshold: 0.2,
+  // });
+
+  // useEffect(() => {
+  //   observer.observe(target.current);
+  // }, [location, offset]);
 
   return (
     <FeedLayout id={id} $feedType="question">
@@ -47,6 +78,7 @@ const FeedPage = () => {
         isLoading={isLoading}
         error={error}
       />
+      {/* {hasNext && <TestBox ref={target} />} */}
       <ModalSection
         subjectId={id}
         handleQuestionSubmit={handleQuestionSubmit}
@@ -56,3 +88,8 @@ const FeedPage = () => {
 };
 
 export default FeedPage;
+
+const TestBox = styled.div`
+  width: 100%;
+  height: 120px;
+`;
